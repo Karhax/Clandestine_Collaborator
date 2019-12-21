29d6d4aff0f3e38cf8fa1d006d156541a6835782
Erik Larsson <karhax@gmail.com>
2019-12-21 04:49:05 +0100

    added darkness, camera masks
    also started on enemy scripts

---
+++ Assets/DarknessSize.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarknessSize : MonoBehaviour
+{
+    SpriteRenderer sr;
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        cam = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+        sr.size = new Vector2(width,height);
+    }
+}
---
+++ Assets/EnemyCameraHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCameraHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    TextMesh cameraID;
+    Transform cameraIDtransform;
+    [SerializeField] string CameraName;
+
+    void Start()
+    {
+        cameraID = GetComponentInChildren<TextMesh>();
+        cameraIDtransform = GetComponentInChildren<TextMesh>().transform;
+        cameraID.text = CameraName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraIDtransform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+    }
+
+    public string ID()
+    {
+        return CameraName;
+    }
+}

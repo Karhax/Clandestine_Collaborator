7c1d52455941884a3c484bcb942ac688c9265af9
Erik Larsson <karhax@gmail.com>
2019-12-30 07:10:32 +0100

    added print command

--- Assets/Console/CommandHandlerScript.cs
+++ Assets/Console/CommandHandlerScript.cs
@@ -10,6 +10,7 @@ public class CommandHandlerScript : MonoBehaviour
     Dictionary<string, string> commandDescription;
 
     bool enableReboot = false;
+    bool enablePrint = false;
 
     void Start()
     {
@@ -18,6 +19,7 @@ public class CommandHandlerScript : MonoBehaviour
         commandDescription.Add("help", "Lists possible commands. Append -? to a command to get a detailed description");
 
         AddRebootCommand();
+        AddPrintCommand();
     }
 
     // Update is called once per frame
@@ -37,6 +39,9 @@ public class CommandHandlerScript : MonoBehaviour
                 break;
             case string a when a.Equals("reboot") && enableReboot:
                 Reboot();
+                break;
+            case string a when a.Equals("print") && enablePrint:
+                Print();
                 break;
             default:
                 UnknownCommand();
@@ -90,7 +95,7 @@ public class CommandHandlerScript : MonoBehaviour
             }
             if (inputList[i].ToLower().Contains("-?"))
             {
-                consoleHistory.AddOutput("Reboot command. Follow reboot with the type of device you wish to reboot.\nTypes are camera.\nExample:\nreboot camera cameraid");
+                consoleHistory.AddOutput("Reboot command. Follow reboot with the type of device you wish to reboot.\nTypes are:\ncamera\nExample:\nreboot camera cameraid");
                 finished = true;
             }
             }
@@ -100,10 +105,61 @@ public class CommandHandlerScript : MonoBehaviour
         }
 
     }
+    void Print()
+    {
+        PrinterHandler[] printers;
+        bool finished = false;
 
+        printers = FindObjectsOfType<PrinterHandler>();
+
+        for (int i = 0; i < inputList.Length; i++)
+        {
+            if (inputList[i].ToLower().Contains("print"))
+            {
+                    if (i + 1 == inputList.Length && !finished)
+                    {
+                        consoleHistory.AddOutput("Could not print, no ID specified.");
+                        finished = true;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < printers.Length; j++)
+                        {
+                            if (inputList[i + 1].ToLower().Equals(printers[j].ID()))
+                            {
+                                printers[j].Print();
+                                consoleHistory.AddOutput("Started printing on Printer with ID '" + inputList[i + 1] + "'.");
+                                finished = true;
+                            }
+                        }
+                        if (!finished && !inputList[i+1].ToLower().Contains("-?"))
+                        {
+                            consoleHistory.AddOutput("Could not find printer with ID '" + inputList[i + 1] + "'.");
+                            finished = true;
+                        }
+                }
+            }
+            if (inputList[i].ToLower().Contains("-?"))
+            {
+                consoleHistory.AddOutput("Printing command that causes a printer to continuously shake and make noise while\ntrying to print an incompatible number of pages.");
+                finished = true;
+            }
+        }
+        if (!finished)
+        {
+            consoleHistory.AddOutput("Could not complete print command");
+        }
+
+    }
     public void AddRebootCommand()
     {
         enableReboot = true;
         commandDescription.Add("reboot", "Reboots a device on the network.");
+    }
+
+    public void AddPrintCommand()
+    {
+        enablePrint = true;
+        commandDescription.Add("print", "Cause a printer to print continuously");
     }
 }
